ecd980d70ba7f6a6c72d3c8e4deedc90f381c0e0
nuclear <2065707071@qq.com>
2024-08-08 00:26:50 +0800

    【add】禁用启用员工功能

--- sky-common/src/main/java/com/sky/result/PageResult.java
+++ sky-common/src/main/java/com/sky/result/PageResult.java
@@ -13,10 +13,10 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PageResult implements Serializable {
+public class PageResult<E> implements Serializable {
 
     private long total; //总记录数
 
-    private List records; //当前页数据集合
+    private List<E> records; //当前页数据集合
 
 }
